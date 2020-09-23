011c43d1bdf1fd391168e39e5ee2a18a7bedfde5
JCho24 <55156654+JCho24@users.noreply.github.com>
2020-09-23 13:12:14 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -12,3 +12,27 @@
 # solution for x
 #
 # test case: 5, 1, 11 should give x = 2
+
+print("The equation ax + b = c is a two step algebra equation")
+print("You can input numbers into a, b, and c to find out what x =")
+
+print("Enter value for a")
+a = input()
+
+print("Enter value for b")
+b = input()
+
+print("Enter value for c")
+c = input()
+
+print("Your equation is setup as " + a + "x + " + b + " = " + c)
+
+a = float(a)
+b = float(b)
+c = float(c)
+
+equation = (c-b)/a
+equation = float(equation)
+
+print("Therefore x = " ,end="")
+print(equation)
